1806437f9b9249219ee9b02d84a99ad4c5c6b10b
PaoloMura <paolo@muraonline.com>
2025-05-23 19:27:24 +0100

    added mobile variant for experience section

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -9,26 +9,58 @@ type ExperienceProps = {
   description: string;
 };
 
-export default function Experience({
+const MobileExperience = ({
   years,
   organisation,
   title,
   description,
-}: ExperienceProps) {
-  return (
-    <Box className={classes.container}>
-      <Box className={classes.header}>
+}: ExperienceProps) => (
+  <Box className={classes.container}>
+    <Box className={classes.mobileHeader}>
+      <TechIcon technology={organisation} size={70} />
+
+      <Box className={classes.mobileTitle}>
+        <Title order={3}>{title}</Title>
         <Text className={classes.years}>{years}</Text>
-
-        <Box className={classes.title}>
-          <TechIcon technology={organisation} size={50} />
-          <Title order={3}>{title}</Title>
-        </Box>
-      </Box>
-
-      <Box className={classes.body}>
-        <Text className={classes.description}>{description}</Text>
       </Box>
     </Box>
+
+    <Text className={classes.mobileDescription}>{description}</Text>
+  </Box>
+);
+
+const DesktopExperience = ({
+  years,
+  organisation,
+  title,
+  description,
+}: ExperienceProps) => (
+  <>
+    <Box className={classes.header}>
+      <Text className={classes.years}>{years}</Text>
+
+      <Box className={classes.title}>
+        <TechIcon technology={organisation} size={50} />
+        <Title order={3}>{title}</Title>
+      </Box>
+    </Box>
+
+    <Box className={classes.body}>
+      <Text className={classes.description}>{description}</Text>
+    </Box>
+  </>
+);
+
+export default function Experience(props: ExperienceProps) {
+  return (
+    <>
+      <Box hiddenFrom="xs">
+        <MobileExperience {...props} />
+      </Box>
+
+      <Box visibleFrom="xs">
+        <DesktopExperience {...props} />
+      </Box>
+    </>
   );
 }
