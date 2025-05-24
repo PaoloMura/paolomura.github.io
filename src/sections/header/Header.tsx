3d7b24130f75f72cb3177807e7aa79926c4aba82
PaoloMura <paolo@muraonline.com>
2025-05-24 15:27:34 +0100

    cleaned up header styles

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -6,8 +6,6 @@ import {
   Drawer,
   Flex,
   NavLink,
-  Space,
-  Stack,
   Switch,
   useComputedColorScheme,
   useMantineColorScheme,
@@ -21,9 +19,35 @@ type HeaderProps = {
   onClickLink: (section: string) => void;
 };
 
-export default function Header({ activeSection, onClickLink }: HeaderProps) {
-  const [opened, { toggle }] = useDisclosure(false);
+type LogoProps = {
+  onClick: () => void;
+};
 
+type LinkProps = {
+  section: string;
+  activeSection: string;
+  onClick: () => void;
+};
+
+const Logo = ({ onClick }: LogoProps) => (
+  <Button onClick={onClick} className={classes.logo} variant="transparent">
+    Paolo Mura
+  </Button>
+);
+
+const Link = ({ section, activeSection, onClick }: LinkProps) => (
+  <Button
+    component="button"
+    onClick={onClick}
+    size="md"
+    variant={activeSection === section ? "selected" : "transparent"}
+    className={classes.link}
+  >
+    {section}
+  </Button>
+);
+
+const ColourSchemeSwitch = () => {
   const { setColorScheme } = useMantineColorScheme();
 
   const computedColorScheme = useComputedColorScheme("light");
@@ -33,72 +57,58 @@ export default function Header({ activeSection, onClickLink }: HeaderProps) {
   };
 
   return (
+    <Switch
+      aria-label="Toggle color scheme"
+      size="xl"
+      checked={computedColorScheme === "dark"}
+      onChange={toggleColorScheme}
+      onLabel={<IconSun size={16} stroke={2.5} />}
+      offLabel={<IconMoon size={16} stroke={2.5} />}
+      thumbIcon={
+        computedColorScheme === "dark" ? (
+          <IconMoon size={16} stroke={2.5} color="black" />
+        ) : (
+          <IconSun size={16} stroke={2.5} color="black" />
+        )
+      }
+      color="gray"
+      classNames={{ track: classes.switch }}
+    />
+  );
+};
+
+export default function Header({ activeSection, onClickLink }: HeaderProps) {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  return (
     <header className={classes.container}>
       <Container size="lg" py="md">
         <Flex justify="space-between" align="center" gap="lg">
-          <Box flex={1}>
-            <Button
-              onClick={() => onClickLink("hero")}
-              className={classes.logo}
-              size="xl"
-              variant="transparent"
-            >
-              Paolo Mura
-            </Button>
-          </Box>
+          <Logo onClick={() => onClickLink("hero")} />
+
+          <Box flex={1} />
 
           <Flex gap="lg" visibleFrom="sm">
-            <Button
-              component="button"
+            <Link
+              section="projects"
+              activeSection={activeSection}
               onClick={() => onClickLink("projects")}
-              className={classes.link}
-              size="md"
-              variant={
-                activeSection === "projects" ? "selected" : "transparent"
-              }
-            >
-              Projects
-            </Button>
+            />
 
-            <Button
-              component="button"
+            <Link
+              section="experiences"
+              activeSection={activeSection}
               onClick={() => onClickLink("experiences")}
-              className={classes.link}
-              size="md"
-              variant={
-                activeSection === "experiences" ? "selected" : "transparent"
-              }
-            >
-              Experience
-            </Button>
+            />
 
-            <Button
-              component="button"
+            <Link
+              section="skills"
+              activeSection={activeSection}
               onClick={() => onClickLink("skills")}
-              className={classes.link}
-              size="md"
-              variant={activeSection === "skills" ? "selected" : "transparent"}
-            >
-              Skills
-            </Button>
+            />
           </Flex>
 
-          <Switch
-            aria-label="Toggle color scheme"
-            size="xl"
-            checked={computedColorScheme === "dark"}
-            onChange={toggleColorScheme}
-            onLabel={<IconSun size={16} stroke={2.5} />}
-            offLabel={<IconMoon size={16} stroke={2.5} />}
-            thumbIcon={
-              computedColorScheme === "dark" ? (
-                <IconMoon size={16} stroke={2.5} color="black" />
-              ) : (
-                <IconSun size={16} stroke={2.5} color="black" />
-              )
-            }
-            color="gray"
-          />
+          <ColourSchemeSwitch />
 
           <Burger
             aria-label="Toggle navigation"
@@ -106,47 +116,31 @@ export default function Header({ activeSection, onClickLink }: HeaderProps) {
             onClick={toggle}
             color="white"
             hiddenFrom="sm"
+            className={classes.burger}
           />
+        </Flex>
+      </Container>
 
-          <Drawer
-            opened={opened}
-            onClose={toggle}
-            position="top"
-            withCloseButton={false}
-            overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
-            transitionProps={{ duration: 500 }}
-            size="30%"
-            hiddenFrom="sm"
-          >
-            <Box className={classes.drawerHeader}>
+      <Drawer
+        opened={opened}
+        onClose={toggle}
+        position="top"
+        withCloseButton={false}
+        overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+        transitionProps={{ duration: 500 }}
+        size="30%"
+        hiddenFrom="sm"
+        styles={{ body: { padding: 0, height: "100%" } }}
+      >
+        <Flex direction="column" h="100%">
+          <Box className={classes.drawerHeader}>
+            <Container size="lg" py="md">
               <Flex justify="space-between" align="center" gap="lg">
                 <Box flex={1}>
-                  <Button
-                    onClick={() => onClickLink("hero")}
-                    className={classes.logo}
-                    size="xl"
-                    variant="transparent"
-                  >
-                    Paolo Mura
-                  </Button>
+                  <Logo onClick={() => onClickLink("hero")} />
                 </Box>
 
-                <Switch
-                  aria-label="Toggle color scheme"
-                  size="xl"
-                  checked={computedColorScheme === "dark"}
-                  onChange={toggleColorScheme}
-                  onLabel={<IconSun size={16} stroke={2.5} />}
-                  offLabel={<IconMoon size={16} stroke={2.5} />}
-                  thumbIcon={
-                    computedColorScheme === "dark" ? (
-                      <IconMoon size={16} stroke={2.5} color="black" />
-                    ) : (
-                      <IconSun size={16} stroke={2.5} color="black" />
-                    )
-                  }
-                  color="gray"
-                />
+                <ColourSchemeSwitch />
 
                 <Burger
                   aria-label="Toggle navigation"
@@ -154,44 +148,44 @@ export default function Header({ activeSection, onClickLink }: HeaderProps) {
                   onClick={toggle}
                   color="white"
                   hiddenFrom="sm"
+                  className={classes.burger}
                 />
               </Flex>
-            </Box>
+            </Container>
+          </Box>
 
-            <Space h="44px" />
-
-            <Stack>
-              <NavLink
-                label="Projects"
-                onClick={() => {
-                  onClickLink("projects");
-                  toggle();
-                }}
-                active={activeSection === "projects"}
-                flex={1}
-              />
-              <NavLink
-                label="Experience"
-                onClick={() => {
-                  onClickLink("experiences");
-                  toggle();
-                }}
-                active={activeSection === "experiences"}
-                flex={1}
-              />
-              <NavLink
-                label="Skills"
-                onClick={() => {
-                  onClickLink("skills");
-                  toggle();
-                }}
-                active={activeSection === "skills"}
-                flex={1}
-              />
-            </Stack>
-          </Drawer>
+          <NavLink
+            label="Projects"
+            onClick={() => {
+              onClickLink("projects");
+              toggle();
+            }}
+            active={activeSection === "projects"}
+            flex={1}
+            className={classes.drawerLink}
+          />
+          <NavLink
+            label="Experience"
+            onClick={() => {
+              onClickLink("experiences");
+              toggle();
+            }}
+            active={activeSection === "experiences"}
+            flex={1}
+            className={classes.drawerLink}
+          />
+          <NavLink
+            label="Skills"
+            onClick={() => {
+              onClickLink("skills");
+              toggle();
+            }}
+            active={activeSection === "skills"}
+            flex={1}
+            className={classes.drawerLink}
+          />
         </Flex>
-      </Container>
+      </Drawer>
     </header>
   );
 }
